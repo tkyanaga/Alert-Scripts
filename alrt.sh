1efe6e7564895ffd059eb4ce2ae8fc0266a52019
tkyanaga <tkyanaga@ucsd.edu>
2019-07-28 22:22:02 -0700

    Added email functionality, smtp must be setup to use

--- alrt.sh
+++ alrt.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
+
+SUBJECT="Alert on $1"
+TOEMAIL=tkyanaga@gmail.com
+MESSAGE="Result count increased for thread $1"
+
+
 pastCount=0
 
 while [ 1 ];
 do
+	#resultCount is # of results on website $1
+
 	resultCount=`curl -s "$1" | grep -h "Results 1 to" | grep -o -E '[0-9]+' | tail -1`
 	if (( resultCount > pastCount ))
 	then
-		echo "Result count increased to $resultCount for thread $1"
+		
+		echo $MESSAGE | mail -s "$SUBJECT" "$TOEMAIL"
+		echo $MESSAGE
+
 	fi
 	pastCount=$resultCount
 	
