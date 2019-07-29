5a0b9fbcd2d1dd1cc6f0fbc86a4cfb9ce3109ab2
tkyanaga <tkyanaga@ucsd.edu>
2019-07-28 23:23:27 -0700

    Utilized a default email and sleep interval

--- alrt.sh
+++ alrt.sh
@@ -2,8 +2,15 @@
 
 
 SUBJECT="Alert on $1"
-TOEMAIL=tkyanaga@gmail.com
-MESSAGE="Result count increased for thread $1"
+TOEMAIL=example@gmail.com
+MESSAGE="Result count increased for thread $1 to "
+
+#sets output email, if exists.. example email is default
+if ! [[ -z "$2" ]]
+then
+    TOEMAIL="$2"
+fi
+
 
 
 pastCount=0
@@ -11,15 +18,16 @@ pastCount=0
 while [ 1 ];
 do
 	#resultCount is # of results on website $1
-
-	resultCount=`curl -s "$1" | grep -h "Results 1 to" | grep -o -E '[0-9]+' | tail -1`
+    resultCount=`curl -s "$1" | grep -h "Results 1 to" | grep -o -E '[0-9]+' | sed '3q;d' `
+#    echo for testing purposes
+#    echo $resultCount
 	if (( resultCount > pastCount ))
 	then
 		
-		echo $MESSAGE | mail -s "$SUBJECT" "$TOEMAIL"
+		echo $MESSAGE $resultCount | mail -s "$SUBJECT" "$TOEMAIL"
 		echo $MESSAGE
 
 	fi
 	pastCount=$resultCount
-	
+	sleep 60
 done
